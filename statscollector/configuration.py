4a9d9f769e39f79c741161ff70231cdddb9cfa84
Antti Jaakkola <antti.jaakkola@finnhems.fi>
2015-07-08 16:18:26 +0300

    local config

--- statscollector/configuration.py
+++ statscollector/configuration.py
@@ -2,3 +2,12 @@ database_username = "statscollector"
 database_password = "changeme"
 database_hostname = "localhost"
 database_database = "statscollector"
+
+
+try:
+    import sys
+    import os
+    sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+    import local_config
+except ImportError:
+    print("Cannot import local settings")
